f35e4f4facacacd5986bcb3af334d9e0f0059de0
ABBAPOH <ABBAPOH@nextmail.ru>
2012-05-31 22:20:10 +0400

    Add CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,28 @@
 #include <QApplication>
+#include <QUrl>
 
+#include "mainwindow.h"
 #include "welcomewindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    QStringList files;
+    foreach (const QString &argument, app.arguments().mid(1)) {
+        QUrl url = QUrl::fromUserInput(argument);
+        if (url.isLocalFile()) {
+            QString file = url.toLocalFile();
+            if (!file.isEmpty())
+                files.append(file);
+        }
+    }
+
+    if (!files.isEmpty()) {
+        MainWindow::open(files);
+        if (!app.topLevelWidgets().isEmpty())
+            return app.exec();
+    }
 
     WelcomeWindow w;
     w.show();
