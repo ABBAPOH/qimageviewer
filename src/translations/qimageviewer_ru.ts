16749bd5b7697aea27be8cca6a607ef7c53e75af
ABBAPOH <ABBAPOH@nextmail.ru>
2012-06-13 22:09:01 +0400

    Update ru ts

--- src/translations/qimageviewer_ru.ts
+++ src/translations/qimageviewer_ru.ts
@@ -2,124 +2,138 @@
 <!DOCTYPE TS>
 <TS version="2.0" language="ru_RU">
 <context>
+    <name>Application</name>
+    <message>
+        <location filename="../application.cpp" line="119"/>
+        <source>Couldn&apos;t restore session (located at %1)</source>
+        <translation>Невозможно восстановить сессию (расположена в %1)</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="68"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="69"/>
+        <source>Can&apos;t restore session</source>
+        <translation>Невозможно восстановить сессию</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="../mainwindow.cpp" line="54"/>
+        <location filename="../mainwindow.cpp" line="112"/>
         <source>About</source>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="55"/>
-        <location filename="../mainwindow.cpp" line="252"/>
-        <location filename="../mainwindow.cpp" line="455"/>
+        <location filename="../mainwindow.cpp" line="113"/>
+        <location filename="../mainwindow.cpp" line="310"/>
+        <location filename="../mainwindow.cpp" line="520"/>
         <source>QImageViewer</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="56"/>
+        <location filename="../mainwindow.cpp" line="114"/>
         <source>Based on Qt %1
 
  (c) Ivan Komissarov aka ABBAPOH</source>
         <translation>Основано на Qt %1\n (c) Иван Комиссаров aka ABBAPOH</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="100"/>
-        <location filename="../mainwindow.cpp" line="434"/>
+        <location filename="../mainwindow.cpp" line="158"/>
+        <location filename="../mainwindow.cpp" line="499"/>
         <source>Open</source>
         <translation>Открыть</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="101"/>
+        <location filename="../mainwindow.cpp" line="159"/>
         <source>You are opening %1 files. Would you like to continue?</source>
         <translation>Вы собираетесь ткрыть %1 файла(ов). Желаете продолжить?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="102"/>
+        <location filename="../mainwindow.cpp" line="160"/>
         <source>Opening large amount of files can cause drop in performance</source>
         <translation>Открытие большого количества файлов может снизить производительность</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="192"/>
-        <location filename="../mainwindow.cpp" line="435"/>
+        <location filename="../mainwindow.cpp" line="250"/>
+        <location filename="../mainwindow.cpp" line="500"/>
         <source>Save</source>
         <translation>Сохранить</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="193"/>
+        <location filename="../mainwindow.cpp" line="251"/>
         <source>File has been modified. Would you like to save it?</source>
         <translation>Файл был изменен. Хотите сохранить?</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="428"/>
+        <location filename="../mainwindow.cpp" line="493"/>
         <source>File</source>
         <translation>Файл</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="429"/>
+        <location filename="../mainwindow.cpp" line="494"/>
         <source>Edit</source>
         <translation>Правка</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="430"/>
+        <location filename="../mainwindow.cpp" line="495"/>
         <source>View</source>
         <translation>Вид</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="431"/>
+        <location filename="../mainwindow.cpp" line="496"/>
         <source>Tools</source>
         <translation>Инструменты</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="432"/>
+        <location filename="../mainwindow.cpp" line="497"/>
         <source>Help</source>
         <translation>Помощь</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="436"/>
+        <location filename="../mainwindow.cpp" line="501"/>
         <source>Save as...</source>
         <translation>Сохранить как...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="437"/>
+        <location filename="../mainwindow.cpp" line="502"/>
         <source>Close</source>
         <translation>Закрыть</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="438"/>
+        <location filename="../mainwindow.cpp" line="503"/>
         <source>Quit</source>
         <translation>Выход</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="439"/>
+        <location filename="../mainwindow.cpp" line="504"/>
         <source>Preferences</source>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="440"/>
+        <location filename="../mainwindow.cpp" line="505"/>
         <source>Resize image...</source>
         <translation>Изменить размер изображения...</translation>
     </message>
     <message>
-        <source>Resize image..</source>
-        <translation type="obsolete">Изменить размер изображения...</translation>
-    </message>
-    <message>
-        <location filename="../mainwindow.cpp" line="441"/>
+        <location filename="../mainwindow.cpp" line="506"/>
         <source>About...</source>
         <translation>О программе...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="442"/>
+        <location filename="../mainwindow.cpp" line="507"/>
         <source>About Qt...</source>
         <translation>О Qt...</translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="451"/>
+        <location filename="../mainwindow.cpp" line="516"/>
         <source>%1* - QImageViewer</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../mainwindow.cpp" line="453"/>
+        <location filename="../mainwindow.cpp" line="518"/>
         <source>%1 - QImageViewer</source>
         <translation></translation>
     </message>
@@ -240,100 +254,105 @@
 <context>
     <name>QImageView</name>
     <message>
-        <location filename="../qimageview.cpp" line="623"/>
+        <location filename="../qimageview.cpp" line="664"/>
         <source>%1 x %2</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="740"/>
+        <location filename="../qimageview.cpp" line="785"/>
         <source>Redo</source>
         <translation>Отменить</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="741"/>
+        <location filename="../qimageview.cpp" line="786"/>
         <source>Undo</source>
         <translation>Повторить</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="742"/>
+        <location filename="../qimageview.cpp" line="787"/>
         <source>Copy</source>
         <translation>Копировать</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="743"/>
+        <location filename="../qimageview.cpp" line="788"/>
         <source>Cut</source>
         <translation>Вырезать</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="744"/>
+        <location filename="../qimageview.cpp" line="789"/>
         <source>Move tool</source>
         <translation>Инструмент перемещения</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="745"/>
+        <location filename="../qimageview.cpp" line="790"/>
         <source>Selection tool</source>
         <translation>Инструмент выделения</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="747"/>
+        <location filename="../qimageview.cpp" line="792"/>
         <source>Zoom in</source>
         <translation>Увеличить</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="748"/>
+        <location filename="../qimageview.cpp" line="793"/>
         <source>Zoom out</source>
         <translation>Уменьшить</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="749"/>
+        <location filename="../qimageview.cpp" line="794"/>
         <source>Fit in view</source>
         <translation>Подогнать под размер окна</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="750"/>
+        <location filename="../qimageview.cpp" line="795"/>
         <source>Normal size</source>
         <translation>Нормальный размер</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="752"/>
+        <location filename="../qimageview.cpp" line="797"/>
         <source>Rotate left</source>
         <translation>Повернуть налево</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="753"/>
+        <location filename="../qimageview.cpp" line="798"/>
         <source>Rotate right</source>
         <translation>Повернуть направо</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="754"/>
+        <location filename="../qimageview.cpp" line="799"/>
         <source>Flip horizontally</source>
         <translation>Отразить горизонтально</translation>
     </message>
     <message>
-        <location filename="../qimageview.cpp" line="755"/>
+        <location filename="../qimageview.cpp" line="800"/>
         <source>Flip vertically</source>
         <translation>Отразить вертикально</translation>
+    </message>
+    <message>
+        <location filename="../qimageview.cpp" line="801"/>
+        <source>Reset original</source>
+        <translation>Вернуть к оригиналу</translation>
     </message>
 </context>
 <context>
     <name>WindowsMenu</name>
     <message>
-        <location filename="../windowsmenu.cpp" line="116"/>
+        <location filename="../windowsmenu.cpp" line="117"/>
         <source>Minimize</source>
         <translation>Уменьшить</translation>
     </message>
     <message>
-        <location filename="../windowsmenu.cpp" line="117"/>
+        <location filename="../windowsmenu.cpp" line="118"/>
         <source>Next window</source>
         <translation>Следующее окно</translation>
     </message>
     <message>
-        <location filename="../windowsmenu.cpp" line="118"/>
+        <location filename="../windowsmenu.cpp" line="119"/>
         <source>Previous window</source>
         <translation>Предыдущее окно</translation>
     </message>
     <message>
-        <location filename="../windowsmenu.cpp" line="120"/>
+        <location filename="../windowsmenu.cpp" line="121"/>
         <source>Windows</source>
         <translation>Окна</translation>
     </message>
